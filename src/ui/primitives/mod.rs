d9298c8112fdb09689d4d662b73e6c21c9208760
Bradley Lyman <lyman.brad3211@gmail.com>
2022-05-20 17:38:48 -0700

    remove the unused line type

--- src/ui/primitives/mod.rs
+++ src/ui/primitives/mod.rs
@@ -1,13 +1,11 @@
 mod dimension_list;
 mod dimensions;
-mod line;
 mod rect;
 mod tile;
 
 pub use self::{
     dimension_list::{Axis, DimensionList, Justify, SpaceBetween},
     dimensions::Dimensions,
-    line::Line,
     rect::Rect,
     tile::Tile,
 };
