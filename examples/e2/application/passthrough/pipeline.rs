3cd5c4c89e3d972e701584e519d9838f6c1bf706
Bradley Lyman <lyman.brad3211@gmail.com>
2022-05-24 23:42:35 -0700

    fix e0-2

--- examples/e2/application/passthrough/pipeline.rs
+++ examples/e2/application/passthrough/pipeline.rs
@@ -158,7 +158,6 @@ pub(super) fn create_pipeline(
     };
     Ok(Pipeline::new_graphics_pipeline(
         pipeline_create_info,
-        msaa_renderpass.render_pass.clone(),
         pipeline_layout,
         vk_dev.clone(),
     )?)
